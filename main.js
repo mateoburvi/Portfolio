771762b7992ed40f71b6d2c5b56295eb536b96d8
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-06-30 15:25:44 -0600

    Fix projects not showing up

--- main.js
+++ main.js
@@ -43,50 +43,52 @@ const projectsArray = [
       'Bootstrap',
       'Terminal',
       'Terminal',
-    ]
+    ],
     liveLink: "https://example.com/project1",
     sourceLink: 'https://github.com/user/project1',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
-   }
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea"
+  }
 ];
 
-for (let i = 0; i < projectsArray.length; i++) {
-  const projectWrapper = document.getElementsByClassName("project_wrapper")[0];
+window.addEventListener('load', () => {
+  for (let i = 0; i < projectsArray.length; i++) {
 
-  const projectCard = document.createElement('article');
-  projectCard.classList.add('project_card', `card${i}`);
+    const projectWrapper = document.getElementsByClassName("project_wrapper")[0];
 
+    const projectCard = document.createElement('article');
+    projectCard.classList.add('project_card', `card${i}`);
 
-  const projectCardImage = document.createElement("img");
-  projectCardImage.classList.add('project__image');
-  projectCardImage.setAttribute("src", projectsArray[i].imgMobile); 
-  projectCardImage.setAttribute('alt', projectsArray[i].name);
-  projectCard.appendChild(projectCardImage);
+    const projectCardImage = document.createElement("img");
+    projectCardImage.classList.add('project_image');
+    projectCardImage.setAttribute("src", projectsArray[i].imgMobile);
+    projectCardImage.setAttribute('alt', projectsArray[i].name);
+    projectCard.appendChild(projectCardImage);
 
-  const projectCardWrapper = document.createElement('div'); 
-  projectCardWrapper.classList.add('project_card_wrapper'); 
-  projectCard.appendChild(projectCardWrapper); 
+    const projectCardWrapper = document.createElement('div');
+    projectCardWrapper.classList.add('project_card_wrapper');
+    projectCard.appendChild(projectCardWrapper);
 
-  const projectCardTitle = document.createElement('h4'); 
-  projectCardTitle.classList.add('project_card_title'); 
-  projectCardTitle.innerHTML = projectsArray[i].name; 
-  projectCardWrapper.appendChild(projectCardTitle);
+    const projectCardTitle = document.createElement('h4');
+    projectCardTitle.classList.add('project_card_title');
+    projectCardTitle.innerText = projectsArray[i].name;
+    projectCardWrapper.appendChild(projectCardTitle);
 
-  const projectCardTags = document.createElement('ul'); 
-  projectCardTags.classList.add('project_card_tags'); 
-  projectCardWrapper.appendChild(projectCardTags);
+    const projectCardTags = document.createElement('ul');
+    projectCardTags.classList.add('project_card_tags');
+    projectCardWrapper.appendChild(projectCardTags);
 
-  for (let j = 0; j < projectsArray[i].techtags.length; j++) {
-    const projectCardTag = document.createElement('li');
-    projectCardTag.innerHTML = projectsArray[i].techtags[j];
-    projectCardTags.appendChild(projectCardTag);
+    for (let j = 0; j < projectsArray[i].techtags.length; j++) {
+      const projectCardTag = document.createElement('li');
+      projectCardTag.innerText = projectsArray[i].techtags[j];
+      projectCardTags.appendChild(projectCardTag);
+    }
+
+    const projectCardButton = document.createElement('button');
+    projectCardButton.classList.add('project_card_button');
+    projectCardButton.setAttribute('type', 'button');
+    projectCardButton.innerText = 'See Project';
+    projectCardWrapper.appendChild(projectCardButton);
+
+    projectWrapper.appendChild(projectCard);
   }
-
-  const projectCardButton = document.createElement('button'); 
-  projectCardButton.classList.add('project_card_button');
-  projectCardButton.setAttribute('type', 'button'); 
-  projectCardButton.innerHTML ='See Project'; 
-  projectCardWrapper.appendChild(projectCardButton);
-
-  projectWrapper.appendChild(projectCard); 
-}
+});
